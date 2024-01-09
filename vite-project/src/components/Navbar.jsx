f2ffb75f513b44fcfebdc932f2d0a2d154982ee6
anshul-59 <anshul.bansod5901@gmail.com>
2024-01-09 17:02:04 +0530

    Shopping Cart project

---
+++ vite-project/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <div>
+        <span className="myshop">SHOPPING</span>
+        <div className="cart">
+          <span>
+            <i className="fas fa-cart-plus"></i>
+          </span>
+        </div>
+      </div>
+    </nav>
+  );
+}
